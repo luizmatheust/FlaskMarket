34bfc7137b8da90adde55c595587b86913e4c286
Luiz Matheus Teixeira Rodrigues <matheus.3223@gmail.com>
2023-07-11 22:07:39 -0300

    - Melhoria no template para exibição de mensagens de validação; - Implementação de validações na classe do formulário de cadastro de usuário; - Testes;

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username:')
-    email_address = StringField(label='Email:')
-    password = PasswordField(label='Password:')
-    password_confirmation = PasswordField(label='Password Confirmation:')
+    username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email:', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password_confirmation = PasswordField(label='Password Confirmation:', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Create Account')
