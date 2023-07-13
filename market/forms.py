b2b9875aad208c4c4c89dc4c301a2de3295c70c7
Luiz Matheus Teixeira Rodrigues <matheus.3223@gmail.com>
2023-07-13 20:32:51 -0300

    - Adicionando métodos de validação dos campos de email e username no formulário de registro de usuário; - Testes;

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('O Usuário já existe! Tente um username diferente.')
+
+    def validate_email_address(self, email_address_to_check):
+        user = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if user:
+            raise ValidationError('Este email já foi cadastrado! Tente um email diferente.')
+
     username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
